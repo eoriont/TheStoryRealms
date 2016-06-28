44ec5147e6570e4c08ba2575c46ed0345337dc9d
mastari <chocolatepie1000@gmail.com>
2016-06-28 09:55:14 -0700

    Again. Just Cleaning more things up and I just changed some small things to make the new mouse and the widgets file works... It is all coming together!

--- src/com/ohmaststudios/assets/Assets.java
+++ src/com/ohmaststudios/assets/Assets.java
@@ -15,13 +15,13 @@ public class Assets {
 
     public void init() {
         blocks.setSpriteSheet(loadImageFrom.LoadImageFrom(this.getClass(), "spritesheet.png"));
-        widgets.setSpriteSheet(loadImageFrom.LoadImageFrom(this.getClass(), "widgets.png"));
+        widgets.setSpriteSheet(loadImageFrom.LoadImageFrom(this.getClass(), "GUI/widgets.png"));
 
         stone1 = blocks.getTile(0, 0, 16, 16);
         wall1 = blocks.getTile(16, 0, 16, 16);
 
-        mouse1 = widgets.getTile(0, 0, 8, 8);
-        mouse2 = widgets.getTile(8, 0, 8, 8);
+        mouse1 = widgets.getTile(0, 0, 5, 5);
+        mouse2 = widgets.getTile(14, 0, 5, 5);
     }
 
 }
